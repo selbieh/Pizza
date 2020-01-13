c10bd78b1318d7e81128e66fa67d09241618e00d
selbieh <selbieh@gmail.com>
2020-01-13 15:45:59 +0200

    better commit

--- pizza/orders/serializer.py
+++ pizza/orders/serializer.py
@@ -7,6 +7,8 @@ from .models import order,orderPizzaItem
 
 class orderObjectsSerializer(ModelSerializer):
 
+    id=serializers.IntegerField(required=False)
+
 
     class Meta:
         fields = ['id','pizza','number','size']
@@ -36,11 +38,15 @@ class orderReadOnlySerializer(ModelSerializer):
         orderObjectLists=validated_data.pop('orderObjects')
         instance.custumerName=validated_data.get('custumerName',instance.custumerName)
         instance.fullAdress=validated_data.get('fullAdress',instance.fullAdress)
-
+        old_pizza_items=instance.orderObjects.all()
+        old_pizza_items_id=[item.id for item in  old_pizza_items]
         new_order_list = []
-        for item in orderObjectLists :
-            create_item,created=orderPizzaItem.objects.get_or_create(**item)
-            new_order_list.append(create_item)
+        orderPizzaItem.objects.filter(id__in=old_pizza_items_id).delete()
+        for item in orderObjectLists:
+            item.pop('id')
+            item=orderPizzaItem.objects.create(**item)
+            new_order_list.append(item)
+        instance.save()
         instance.orderObjects.set(new_order_list)
         return instance
 
